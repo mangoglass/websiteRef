710f446c734d71dd80ba05c1833c05f3a506dd71
Tom Axblad <axblad@kth.se>
2018-05-02 22:17:02 +0200

    Add dropdown menu

--- GolfOnePager/script.js
+++ GolfOnePager/script.js
@@ -2,4 +2,24 @@ $(document).ready(function(){
     $(".navElem").click(function(){
         $(this).animate({top: '-5px'}, 100, 'linear').animate({top: '5px'}, 100, 'linear').animate({top: '0px'}, 100, 'linear');
     });
+
+    $dropped = false;
+    $dropping = false;
+
+    $("#lines").click(function(){
+          if(!$dropping) {
+                $dropping = true;
+                if(!$dropped) {
+                      $("#dropdown").animate({top: '70px'}, 300, function() {
+                           $dropped = true;
+                           $dropping = false;
+                     });
+                } else {
+                      $("#dropdown").animate({top: '-325px'}, 300, function() {
+                           $dropped = false;
+                           $dropping = false;
+                     });
+                }
+          }
+    });
 });
